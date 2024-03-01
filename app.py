e6a10bdd7e9e227a45a97d0f7807d72966a595aa
Felipe <mac@MacBook-Pro-de-Mac.local>
2024-03-01 19:35:17 -0300

    separando em métodos

--- app.py
+++ app.py
@@ -1,35 +1,43 @@
 import os
 
-print("hello world")
-
-print("1. Cadastrar Restaurante")
-
-print("2. Listar Restaurantes")
-print("3. Ativar Restaurantes")
-
-opcao_escolhida = input('Digite a opção desejada: ')
-opcao_escolhida = int(opcao_escolhida)
-print(opcao_escolhida == 1)
-print (type(opcao_escolhida))
-print(type(1))
-
-print('Você escolheu a seguinte opção', opcao_escolhida)
-
-print(f'Você escolheu a opção {opcao_escolhida}')
 
 
+def exibir_nome_do_programa():
+    print("hello world")
+
+
+def exibir_opcoes():
+    print("1. Cadastrar Restaurante")
+    print("2. Listar Restaurantes")
+    print("3. Ativar Restaurantes")
 
 def finalizarApp():
-    os.system('clear')
     print('Finalizando aplicação')
+    os.system('clear')
 
 
+def escolher_opcao():
+    opcao_escolhida = input('Digite a opção desejada: ')
+    opcao_escolhida = int(opcao_escolhida)
+    print(opcao_escolhida == 1)
+    print (type(opcao_escolhida))
+    print(type(1))
+    print(f'Você escolheu a opção {opcao_escolhida}')
 
-if opcao_escolhida == '1':
-    print('Cadastrar Restaurante')
-elif opcao_escolhida == '2':
-    print('Listar Restaurantes')
-elif opcao_escolhida == '3':
-    print('Ativar Restaurantes')
-else :
-    finalizarApp()
+    if opcao_escolhida == 1:
+        print('Cadastrar Restaurante')
+    elif opcao_escolhida == 2:
+        print('Listar Restaurantes')
+    elif opcao_escolhida == 3:
+        print('Ativar Restaurantes')
+    else :
+        finalizarApp()
+
+
+def main():
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcao()
+
+if __name__ == "__main__":
+   main()
