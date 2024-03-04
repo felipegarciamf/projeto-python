9f0d0324f5825f8c1e670c858cd4ce93f4ae99ab
Felipe <mac@MacBook-Pro-de-Mac.local>
2024-03-04 14:44:00 -0300

    subindo desafio logon

---
+++ desafio_logon.py
@@ -0,0 +1,11 @@
+
+usuario = input("Digita seu usuario: ")   
+senha = input("Digite sua senha: ")
+
+usuario = int(usuario)
+senha = int(senha)
+
+if usuario == 123 and senha == 123:
+    print("Bem vindo")
+else:
+    print("Usuário ou senha inválidos")
