f8ccd8d0949a92727ff8254fa46bb89a92d4552a
Felipe <mac@MacBook-Pro-de-Mac.local>
2024-03-05 20:38:12 -0300

    realizando desafio de lista

---
+++ desafio_lista_1_a_10.py
@@ -0,0 +1,23 @@
+
+
+def listar_numeros():
+    for numero in range(1, 11):
+        print(numero)
+
+def listar_quatro_nomes():
+    nomes = ['João', 'Maria', 'José', 'Ana']
+    for nome in nomes:
+        print(nome)
+
+def listar_ano_que_nasci_e_ano_atual(): 
+    ano_nascimento = 1989
+    ano_atual = 2024
+
+    for ano in range(ano_nascimento, ano_atual):
+        print(ano)
+
+
+listar_numeros()
+listar_quatro_nomes()
+listar_ano_que_nasci_e_ano_atual()
+
