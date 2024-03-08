69918421d65fe797e2ac0b7524330ceefce865cd
Felipe <mac@MacBook-Pro-de-Mac.local>
2024-03-08 06:03:56 -0300

    criando classe musica

---
+++ curso2-orientacao-objeto/musica.py
@@ -0,0 +1,24 @@
+class Musica:
+    nome = ''
+    artista = ''
+    duracao = ''
+
+musica1 = Musica()
+musica1.nome = 'The Scientist'
+musica1.artista = 'Coldplay'
+musica1.duracao = '5:09'
+
+musica2 = Musica()
+musica2.nome = 'Yellow'
+musica2.artista = 'Coldplay'
+musica2.duracao = '4:29'
+
+musica3 = Musica()
+musica3.nome = 'Fix You'
+musica3.artista = 'Coldplay'
+musica3.duracao = '4:55'
+
+
+musicas = [vars(musica1), vars(musica2), vars(musica3)]
+
+print(musicas)
