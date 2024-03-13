98475037723564a31f07df5b66c7f06902eab697
Felipe <mac@MacBook-Pro-de-Mac.local>
2024-03-13 09:57:34 -0300

    criando construtor

--- curso2-orientacao-objeto/musica.py
+++ curso2-orientacao-objeto/musica.py
@@ -5,27 +5,9 @@ class Musica:
         self.duracao = duracao
 
 
-
-    nome = ''
-    artista = ''
-    duracao = ''
-
-musica1 = Musica()
-musica1.nome = 'The Scientist'
-musica1.artista = 'Coldplay'
-musica1.duracao = '5:09'
-
-musica2 = Musica()
-musica2.nome = 'Yellow'
-musica2.artista = 'Coldplay'
-musica2.duracao = '4:29'
-
-musica3 = Musica()
-musica3.nome = 'Fix You'
-musica3.artista = 'Coldplay'
-musica3.duracao = '4:55'
+musica1 = Musica('The Scientist', 'Coldplay', '5:09')
 
 
-musicas = [vars(musica1), vars(musica2), vars(musica3)]
+musicas = [vars(musica1)]
 
 print(musicas)
