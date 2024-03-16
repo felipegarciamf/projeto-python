3ab1dcc38b55ef364465e84320c418cd9cc90a1e
Felipe <mac@MacBook-Pro-de-Mac.local>
2024-03-16 07:25:53 -0300

    utilziando metodo str

--- curso2-orientacao-objeto/musica.py
+++ curso2-orientacao-objeto/musica.py
@@ -3,11 +3,40 @@ class Musica:
         self.nome = nome
         self.artista = artista
         self.duracao = duracao
+    def __str__(self):
+        return self.nome
 
 
 musica1 = Musica('The Scientist', 'Coldplay', '5:09')
 
 
+print(musica1)
 musicas = [vars(musica1)]
 
 print(musicas)
+
+
+
+class Teste:
+    def __init__(self, teste):
+        self.teste = teste
+    def __srt__(self):
+        return self.teste
+
+
+
+teste = Teste('teste do sileira')
+
+
+print(teste)
+print(vars(teste))
+
+
+
+def cadastro_musicao(nome, artista, duracao):
+    musica = Musica(nome, artista, duracao)
+    print(vars(musica))
+
+
+
+## cadastro_musicao("joseph", "teste", "vai")
