8a535ce2eafeb78af5b2840e5e68ce330691af32
Felipe <mac@MacBook-Pro-de-Mac.local>
2024-03-16 08:05:21 -0300

    colocaondo atributos como privado incluindo _

--- curso3-property/resturante.py
+++ curso3-property/resturante.py
@@ -1,12 +1,12 @@
 class Restaurante:
     restaurantes = []
     def __init__(self, nome, categoria):
-        self.nome = nome.title()
-        self.categoria = categoria.upper()
+        self._nome = nome.title()
+        self._categoria = categoria.upper()
         self._ativo = False
         Restaurante.restaurantes.append(self)
     def __str__(self):
-        return f'{self.nome} ({self.categoria} | {self.ativo})'
+        return f'{self._nome} ({self._categoria} | {self.ativo})'
     def listar_restaurantes():
         for restaurante in Restaurante.restaurantes:
             print(restaurante)
