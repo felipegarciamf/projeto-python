1b378bd4870fce6e38fef5ef3b86b35d694a767e
Felipe <mac@MacBook-Pro-de-Mac.local>
2024-03-20 19:52:11 -0300

    criando classe com composição de classes para chamada interna com avaliacao e listagem por resturante'

--- curso4-from-import/modelos/restaurante.py
+++ curso4-from-import/modelos/restaurante.py
@@ -1,3 +1,6 @@
+from modelos.avaliacao import Avaliacao
+
+
 class Restaurante:
     restaurantes = []
     def __init__(self, nome, categoria):
@@ -5,6 +8,9 @@ class Restaurante:
         self._categoria = categoria.upper()
         self._ativo = False
         Restaurante.restaurantes.append(self)
+        self.avalicao = []
+
+
     def __str__(self):
         return f'{self._nome} ({self._categoria} | {self._ativo})'
     
@@ -13,6 +19,9 @@ class Restaurante:
     def listar_restaurantes(cls):
         for restaurante in cls.restaurantes:
             print(restaurante)
+            for avaliacao in restaurante.avalicao:
+                print(avaliacao)
+
 
     def alterar_status(self):
         self._ativo = not self._ativo
@@ -21,14 +30,7 @@ class Restaurante:
     def ativo(self):
         return 'verdadeiro' if self._ativo else 'false'
     
-
-
-restaurante_praca2 = Restaurante('Restaurante da Praça','Comida Brasileira')
-restaurante_praca2.alterar_status()
-
-
-
-restaurante_pizza2 = Restaurante('Pizzaria', 'Comida Italiana')
-
-restaurante_pizza2.listar_restaurantes()
-
+    def adicionar_avaliacao(self, nome, nota):
+        avaliacao = Avaliacao(nome, nota)
+        self.avalicao.append(avaliacao)
+        
